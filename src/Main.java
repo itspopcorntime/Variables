b57e0a381b2a253386e46402889ea770d18fa381
sam spoth <sams73037@gmail.com>
2022-09-09 09:02:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        char c = 97;
+        System.out.println(c);
+
+        double d = 9.7;
+        System.out.println(d);
+    }
+}
