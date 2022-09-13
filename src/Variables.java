be0ebb6ff11ba0697cfdc110b7c70f6cb6d784ca
sam spoth <sams73037@gmail.com>
2022-09-13 08:24:37 -0500

    Defined and Printed variables

---
+++ src/Variables.java
@@ -0,0 +1,40 @@
+public class Variables {
+    public static void main ( String[] args )
+        {
+            //define 1 variable of each of the
+            //following data types
+            //byte	short		int 		long
+            byte value = 32;
+            short valueTwo = -1567;
+            int valueThree = 234574;
+            long hugeValue = 73294784;
+
+            //float	double
+
+            float realValue = 4.67f;
+            double otherRealValue = -574.8549;
+
+            //char      boolean	String
+
+            char d = 65;
+            boolean hasDog = false;
+
+            String className = "AP Computer Science";
+
+            //integer variables
+            byte byteOne = 127;
+
+
+
+            System.out.println(value);
+            System.out.println(valueTwo);
+            System.out.println(valueThree);
+            System.out.println(hugeValue);
+            System.out.println(realValue);
+            System.out.println(otherRealValue);
+            System.out.println(d);
+            System.out.println(hasDog);
+            System.out.println(className);
+
+        }
+    }
